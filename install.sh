ee34a1badd86a7c63b2ff094dd73d7e9cc403d92
RRkundkar77 <r4002001005025k@gmail.com>
2021-01-21 15:10:29 +0530

    Gnome Desktop Shortcut Added

---
+++ install.sh
@@ -0,0 +1,9 @@
+cp -r v1.0.0_linux $HOME/RNote
+cp $HOME/RNote/RNote.desktop $HOME/Desktop/RNote.desktop
+cp ./Images/RNote.png $HOME/RNote/RNote.png
+
+
+sed -i "4i4 Exec=$HOME/RNote/RNote/" $HOME/Desktop/RNote.desktop
+sed -i "5i5 Icon=$HOME/RNote/Images/RNote.png/" $HOME/Desktop/RNote.desktop 
+
+echo "RNote Installed Successfully\n"
