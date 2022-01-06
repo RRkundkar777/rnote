143c0961e3a54cfac64f9b569a4be1a214d4fb30
rushikesh <r4002001005025k@gmail.com>
2022-01-06 11:42:43 +0530

    added app icon

---
+++ builds/build-debug-linux/CMakeFiles/qt5app.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/rushikesh/MacOS/MTXOS/desktop-projects/rnote/builds/build-debug-linux/qt5app_autogen/mocs_compilation.cpp" "/media/rushikesh/MacOS/MTXOS/desktop-projects/rnote/builds/build-debug-linux/CMakeFiles/qt5app.dir/qt5app_autogen/mocs_compilation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_MESSAGELOGCONTEXT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../../"
+  "qt5app_autogen/include"
+  "../../forms"
+  "../../headers"
+  "../../src"
+  "../../resources"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- builds/build-debug-linux/rnote_autogen/K6S233IZAZ/moc_rwindow.cpp
+++ builds/build-debug-linux/rnote_autogen/K6S233IZAZ/moc_rwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_RWindow_t {
-    QByteArrayData data[14];
-    char stringdata0[308];
+    QByteArrayData data[16];
+    char stringdata0[366];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -38,24 +38,28 @@ QT_MOC_LITERAL(3, 32, 23), // "on_actionOpen_triggered"
 QT_MOC_LITERAL(4, 56, 23), // "on_actionSave_triggered"
 QT_MOC_LITERAL(5, 80, 26), // "on_actionSave_As_triggered"
 QT_MOC_LITERAL(6, 107, 26), // "on_actionSupport_triggered"
-QT_MOC_LITERAL(7, 134, 35), // "on_tabWidget_Note_tabCloseReq..."
-QT_MOC_LITERAL(8, 170, 5), // "index"
-QT_MOC_LITERAL(9, 176, 31), // "on_tabWidget_Note_tabBarClicked"
-QT_MOC_LITERAL(10, 208, 23), // "on_actionFont_triggered"
-QT_MOC_LITERAL(11, 232, 23), // "on_actionDark_triggered"
-QT_MOC_LITERAL(12, 256, 24), // "on_actionLight_triggered"
-QT_MOC_LITERAL(13, 281, 26) // "on_actionMonokai_triggered"
+QT_MOC_LITERAL(7, 134, 24), // "on_actionAbout_triggered"
+QT_MOC_LITERAL(8, 159, 35), // "on_tabWidget_Note_tabCloseReq..."
+QT_MOC_LITERAL(9, 195, 5), // "index"
+QT_MOC_LITERAL(10, 201, 31), // "on_tabWidget_Note_tabBarClicked"
+QT_MOC_LITERAL(11, 233, 23), // "on_actionFont_triggered"
+QT_MOC_LITERAL(12, 257, 23), // "on_actionDark_triggered"
+QT_MOC_LITERAL(13, 281, 24), // "on_actionLight_triggered"
+QT_MOC_LITERAL(14, 306, 26), // "on_actionMonokai_triggered"
+QT_MOC_LITERAL(15, 333, 32) // "on_actionSend_Feedback_triggered"
 
     },
     "RWindow\0on_actionNew_triggered\0\0"
     "on_actionOpen_triggered\0on_actionSave_triggered\0"
     "on_actionSave_As_triggered\0"
     "on_actionSupport_triggered\0"
+    "on_actionAbout_triggered\0"
     "on_tabWidget_Note_tabCloseRequested\0"
     "index\0on_tabWidget_Note_tabBarClicked\0"
     "on_actionFont_triggered\0on_actionDark_triggered\0"
     "on_actionLight_triggered\0"
-    "on_actionMonokai_triggered"
+    "on_actionMonokai_triggered\0"
+    "on_actionSend_Feedback_triggered"
 };
 #undef QT_MOC_LITERAL
 
@@ -65,7 +69,7 @@ static const uint qt_meta_data_RWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      11,   14, // methods
+      13,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -73,17 +77,19 @@ static const uint qt_meta_data_RWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   69,    2, 0x08 /* Private */,
-       3,    0,   70,    2, 0x08 /* Private */,
-       4,    0,   71,    2, 0x08 /* Private */,
-       5,    0,   72,    2, 0x08 /* Private */,
-       6,    0,   73,    2, 0x08 /* Private */,
-       7,    1,   74,    2, 0x08 /* Private */,
-       9,    1,   77,    2, 0x08 /* Private */,
-      10,    0,   80,    2, 0x08 /* Private */,
-      11,    0,   81,    2, 0x08 /* Private */,
-      12,    0,   82,    2, 0x08 /* Private */,
-      13,    0,   83,    2, 0x08 /* Private */,
+       1,    0,   79,    2, 0x08 /* Private */,
+       3,    0,   80,    2, 0x08 /* Private */,
+       4,    0,   81,    2, 0x08 /* Private */,
+       5,    0,   82,    2, 0x08 /* Private */,
+       6,    0,   83,    2, 0x08 /* Private */,
+       7,    0,   84,    2, 0x08 /* Private */,
+       8,    1,   85,    2, 0x08 /* Private */,
+      10,    1,   88,    2, 0x08 /* Private */,
+      11,    0,   91,    2, 0x08 /* Private */,
+      12,    0,   92,    2, 0x08 /* Private */,
+      13,    0,   93,    2, 0x08 /* Private */,
+      14,    0,   94,    2, 0x08 /* Private */,
+      15,    0,   95,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -91,8 +97,10 @@ static const uint qt_meta_data_RWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Int,    8,
-    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    9,
+    QMetaType::Void, QMetaType::Int,    9,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -112,12 +120,14 @@ void RWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, voi
         case 2: _t->on_actionSave_triggered(); break;
         case 3: _t->on_actionSave_As_triggered(); break;
         case 4: _t->on_actionSupport_triggered(); break;
-        case 5: _t->on_tabWidget_Note_tabCloseRequested((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 6: _t->on_tabWidget_Note_tabBarClicked((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 7: _t->on_actionFont_triggered(); break;
-        case 8: _t->on_actionDark_triggered(); break;
-        case 9: _t->on_actionLight_triggered(); break;
-        case 10: _t->on_actionMonokai_triggered(); break;
+        case 5: _t->on_actionAbout_triggered(); break;
+        case 6: _t->on_tabWidget_Note_tabCloseRequested((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 7: _t->on_tabWidget_Note_tabBarClicked((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 8: _t->on_actionFont_triggered(); break;
+        case 9: _t->on_actionDark_triggered(); break;
+        case 10: _t->on_actionLight_triggered(); break;
+        case 11: _t->on_actionMonokai_triggered(); break;
+        case 12: _t->on_actionSend_Feedback_triggered(); break;
         default: ;
         }
     }
@@ -148,13 +158,13 @@ int RWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 11)
+        if (_id < 13)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 11;
+        _id -= 13;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 11)
+        if (_id < 13)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 11;
+        _id -= 13;
     }
     return _id;
 }
